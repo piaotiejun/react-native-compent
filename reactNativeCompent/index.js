8a8eef6ebf92b151b5c1fa22c1e1e18e082057f9
piaotiejun <674216231@qq.com>
2018-03-14 09:52:50 +0800

    helloworld

---
+++ reactNativeCompent/index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+import HelloWorldApp from './Main/HelloWorld';
+
+
+AppRegistry.registerComponent('reactNativeCompent', () => HelloWorldApp);
