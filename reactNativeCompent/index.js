e8163e9f5d6c64e5c173a292939f28dab1737e5d
piaotiejun <674216231@qq.com>
2018-03-14 11:19:39 +0800

    添加高德库自带example

--- reactNativeCompent/index.js
+++ reactNativeCompent/index.js
@@ -3,6 +3,7 @@ import { AppRegistry } from 'react-native';
 import HelloWorldApp from './Main/HelloWorld';
 import MyActivityIndicator from './Main/MyActivityIndicator';
 import GaodeMapTest from './Main/GaodeMapTest';
+import GaodeMapTest1 from './Main/GaodeExample/index';
 
 
-AppRegistry.registerComponent('reactNativeCompent', () => GaodeMapTest);
+AppRegistry.registerComponent('reactNativeCompent', () => GaodeMapTest1);
